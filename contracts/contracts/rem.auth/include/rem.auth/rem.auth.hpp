542d31092017bdf8fab28b30835a262469e24f79
Ember <vlader112@gmail.com>
2019-09-23 19:34:26 +0300

    delete deferred action logic && refactor code

--- contracts/contracts/rem.auth/include/rem.auth/rem.auth.hpp
+++ contracts/contracts/rem.auth/include/rem.auth/rem.auth.hpp
@@ -6,6 +6,7 @@
 
 #include <eosio/asset.hpp>
 #include <eosio/crypto.hpp>
+#include <eosio/singleton.hpp>
 #include <eosio/eosio.hpp>
 #include <eosio/transaction.hpp>
 #include <eosio/permission.hpp>
@@ -32,7 +33,7 @@ namespace eosio {
       auth(name receiver, name code,  datastream<const char*> ds);
 
       /**
-       * Add new authentication key.
+       * Add new authentication key action.
        *
        * @details Add new authentication key by user account.
        *
@@ -40,16 +41,16 @@ namespace eosio {
        * @param key - the public key that signed the payload,
        * @param signed_by_key - the signature that sign payload by key,
        * @param extra_key - the public key for authorization in external services,
-       * @param payer_str - the account from which resources are deducted.
+       * @param payer_str - the account from which resources are debited.
        */
       [[eosio::action]]
       void addkeyacc(const name &account, const public_key &key, const signature &signed_by_key,
                      const string &extra_key, const string &payer_str);
 
       /**
-       * Add new device key.
+       * Add new authentication key action.
        *
-       * @details Add new authentication device.
+       * @details Add new authentication key by using correspond to account authentication key.
        *
        * @param account - the owner account to execute the addkeyacc action for,
        * @param new_key - the public key that will be added,
@@ -57,21 +58,38 @@ namespace eosio {
        * @param extra_key - the public key for authorization in external services,
        * @param key - the public key which is tied to the corresponding account,
        * @param sign_by_key - the signature that sign payload by key,
-       * @param payer_str - the account from which resources are deducted.
+       * @param payer_str - the account from which resources are debited.
        */
       [[eosio::action]]
       void addkeyapp(const name &account, const public_key &new_key, const signature &signed_by_new_key,
                      const string &extra_key, const public_key &key, const signature &signed_by_key,
                      const string &payer_str);
 
-
+      /**
+       * Transfer action.
+       *
+       * @details Allows `from` account to transfer to `to` account the `quantity` tokens.
+       * One account is debited and the other is credited with quantity tokens.
+       *
+       * @param from - the account to transfer from,
+       * @param to - the account to be transferred to,
+       * @param quantity - the quantity of tokens to be transferred,
+       * @param key - the public key which is tied to the corresponding account,
+       * @param signed_by_key - the signature that sign payload by key,
+       */
       [[eosio::action]]
-      void addkeywrap(const name &account, const public_key &key, const string &extra_key, const name &payer);
+      void transfer(const name &from, const name &to, const asset &quantity,
+                    const public_key &key, const signature &signed_by_key);
 
-
+      /**
+       * Set block producers reward.
+       *
+       * @details Change amount of block producers reward, action permitted only for producers.
+       *
+       * @param quantity - the quantity of tokens to be rewarded.
+       */
       [[eosio::action]]
-      void transfer( const name &from, const name &to, const asset &quantity,
-                     const public_key &key, const signature &signed_by_key );
+      void setbpreward(const asset &quantity);
 
 
       [[eosio::action]]
@@ -80,16 +98,12 @@ namespace eosio {
       using addkeyacc_action = action_wrapper<"addkeyacc"_n, &auth::addkeyacc>;
       using addkeyapp_action = action_wrapper<"addkeyapp"_n, &auth::addkeyapp>;
       using transfer_action = action_wrapper<"transfer"_n, &auth::transfer>;
-      using addkeywrap_action = action_wrapper<"addkeywrap"_n, &auth::addkeywrap>;
 
    private:
       static constexpr symbol core_symbol{"REM", 4};
       static constexpr name system_account = "rem"_n;
       static constexpr name system_token_account = "rem.token"_n;
-      const asset prods_reward{100000, core_symbol};
       const time_point key_lifetime = time_point(seconds(31104000)); // 360 days
-      const uint32_t wait_confirm_sec = 86400; // 1 day
-      const time_point wait_confirm_time = time_point(seconds(wait_confirm_sec));
 
       struct [[eosio::table]] authkeys {
          uint64_t          N;
@@ -106,49 +120,30 @@ namespace eosio {
          EOSLIB_SERIALIZE( authkeys, (key)(owner)(key)(extra_key)(not_valid_before)(not_valid_after)(revoked_at))
       };
 
+      struct [[eosio::table]] prodsreward {
+         asset quantity{500000, core_symbol};
+
+         // explicit serialization macro is not necessary, used here only to improve compilation time
+         EOSLIB_SERIALIZE( prodsreward, (quantity))
+      };
+
       typedef multi_index<"authkeys"_n, authkeys,
                           indexed_by<"byname"_n, const_mem_fun < authkeys, uint64_t, &authkeys::by_name>>
                           > authkeys_inx;
+
+      typedef singleton<"prodsreward"_n, prodsreward> p_reward;
+
       authkeys_inx authkeys_tbl;
+      p_reward p_reward_table;
+      prodsreward prods_reward;
 
-      struct [[eosio::table]] wait_confirm {
-         uint64_t              key;
-         name                  owner;
-         block_timestamp       init_time;
-         block_timestamp       wait_time;
-         name                  action_account;
-         name                  action_name;
-         vector<char>          action_data;
-         vector<public_key>    provided_approvals;
-
-         uint64_t primary_key() const { return key; }
-         uint64_t by_name() const { return owner.value; }
-
-         EOSLIB_SERIALIZE( wait_confirm, (key)(owner)(init_time)(wait_time)(action_account)
-                                         (action_name)(action_data)(provided_approvals))
-      };
-
-      typedef multi_index<"wait"_n, wait_confirm,
-                          indexed_by<"byname"_n, const_mem_fun < wait_confirm, uint64_t, &wait_confirm::by_name>>
-                          > wait_confirm_idx;
-      wait_confirm_idx wait_confirm_tbl;
-
-      string join(vector <string> &&vec, string delim = string("*"));
-      void to_rewards(const name& payer, const asset &quantity);
+      void _addkey( const name& account, const public_key& device_key, const string& extra_key, const name& payer);
       void require_app_auth( const checksum256 &digest, const name &user,
                              const public_key &sign_pub_key, const signature &signature );
 
-      template<class T>
-      void boost_deferred_tx(const T& it, const action& acc, const public_key& device_key);
-      void send_deferred_tx(const action &act, const uint32_t &delay, const uint128_t &id);
+      void to_rewards(const name& payer, const asset &quantity);
 
-      void _addkey( const name& account, const public_key& device_key, const string& extra_key, const name& payer);
-
-      void add_wait_action(const name &account, const name &action_account, const name &action_name,
-                           const vector<char> action_data, const public_key &sign_pubkey, const time_point &ct);
-      auto get_wait_action_it(const name &account, const name &action_account,
-                              const name &action_name, const vector<char> &action_data);
-
+      string join(vector <string> &&vec, string delim = string("*"));
       checksum256 sha256(const string &str) {
          return eosio::sha256(str.c_str(), str.size());
       }
--- contracts/contracts/rem.auth/src/rem.auth.cpp
+++ contracts/contracts/rem.auth/src/rem.auth.cpp
@@ -7,9 +7,16 @@
 #include <rem.token/rem.token.hpp>
 
 namespace eosio {
-   auth::auth(name receiver, name code,  datastream<const char*> ds): contract(receiver, code, ds),
-                                                                      authkeys_tbl(_self, _self.value),
-                                                                      wait_confirm_tbl(_self, _self.value) {};
+   auth::auth(name receiver, name code,  datastream<const char*> ds)
+   :contract(receiver, code, ds),
+    authkeys_tbl(_self, _self.value),
+    p_reward_table(_self, _self.value)
+    {
+       if ( !p_reward_table.exists() ) {
+          p_reward_table.set(prodsreward{}, _self);
+       }
+       prods_reward = p_reward_table.get();
+    };
 
    void auth::addkeyacc(const name &account, const public_key &key, const signature &signed_by_key,
                         const string &extra_key, const string &payer_str) {
@@ -25,85 +32,46 @@ namespace eosio {
       _addkey(account, key, extra_key, payer);
    }
 
-   auto auth::get_wait_action_it(const name &account, const name &action_account,
-                                 const name &action_name, const vector<char> &action_data) {
-      auto wait_idx = wait_confirm_tbl.get_index<"byname"_n>();
-      auto it = wait_idx.find(account.value);
-      if (it == wait_idx.end()) { return it; }
-
-      auto ct = current_time_point();
-      for (; it != wait_idx.end(); ++it) {
-         bool is_executed = ct > it->wait_time;
-         bool is_equal_action_acc = it->action_account == action_account;
-         bool is_equal_action_name = it->action_name == action_name;
-
-         if (  is_executed || !is_equal_action_acc || !is_equal_action_name ) {
-            continue;
-         } else if (it->action_data == action_data) {
-            break;
-         }
-      }
-      return it;
-   }
-
    void auth::addkeyapp(const name &account, const public_key &new_key, const signature &signed_by_new_key,
                         const string &extra_key, const public_key &key, const signature &signed_by_key,
                         const string &payer_str) {
 
-      auto wait_idx = wait_confirm_tbl.get_index<"byname"_n>();
       checksum256 digest = sha256(join( { account.to_string(), string(new_key.data.begin(), new_key.data.end()),
-                                          string(key.data.begin(), key.data.end()), extra_key,
+                                          extra_key, string(key.data.begin(), key.data.end()),
                                           payer_str } ));
       name payer = payer_str.empty() ? account : name(payer_str);
       require_app_auth(digest, account, key, signed_by_key);
-      assert_recover_key(digest, signed_by_new_key, new_key);
 
-      addkeywrap_action addkeywrap(_self, { _self, "active"_n });
-      action addkey_action = addkeywrap.to_action(account, new_key, extra_key, payer);
-      auto it = get_wait_action_it(account, _self, "addkeywrap"_n, addkey_action.data);
+      public_key expected_new_key = recover_key(digest, signed_by_new_key);
+      check(expected_new_key == new_key, "expected key different than recovered new key");
 
-      if (it != wait_idx.end()) {
-         boost_deferred_tx(it, addkey_action, key);
-      } else {
-         const time_point ct = current_time_point();
-         add_wait_action(account, _self, "addkeywrap"_n, addkey_action.data, key, ct);
-         send_deferred_tx(addkey_action, wait_confirm_sec, ct.sec_since_epoch() + account.value);
-      }
+      _addkey(account, new_key, extra_key, payer);
    }
 
-   template<class T>
-   void auth::boost_deferred_tx(const T &it, const action &act, const public_key &key) {
-      bool is_already_approved = std::find( it->provided_approvals.begin(),
-                                            it->provided_approvals.end(), key
-                                            ) == it->provided_approvals.end();
-      check(is_already_approved, "approval already exists");
+   void auth::transfer(const name &from, const name &to, const asset &quantity,
+                       const public_key &key, const signature &signed_by_key) {
 
-      const time_point ct = current_time_point();
-      uint32_t delta = it->wait_time.to_time_point().sec_since_epoch() - ct.sec_since_epoch();
-      check(delta >= 0, "action already executed");
+      checksum256 digest = sha256(join( { from.to_string(), to.to_string(), quantity.to_string(),
+                                          string(key.data.begin(), key.data.end()) } ));
 
-      uint32_t wait_sec = delta / (it->provided_approvals.size() + 1);
-      time_point wait_time = time_point(seconds(wait_sec));
+      require_app_auth(digest, from, key, signed_by_key);
 
-      wait_confirm_tbl.modify(*it, _self, [&](auto &w) { // TODO: change _self to it->owner
-         w.provided_approvals.push_back(key);
-         w.wait_time = it->init_time.to_time_point() + wait_time;
-      });
-
-      uint128_t deferred_tx_id = it->init_time.to_time_point().sec_since_epoch() + it->owner.value;
-      cancel_deferred(deferred_tx_id);
-      send_deferred_tx(act, wait_sec, deferred_tx_id);
+      token::transfer_action transfer(system_token_account, {from, "active"_n});
+      transfer.send(from, to, quantity, string("authentication app transfer"));
    }
 
-   void auth::addkeywrap(const name &account, const public_key &key, const string &extra_key, const name &payer) {
-      require_auth(_self);
-      _addkey(account, key, extra_key, payer);
+   void auth::setbpreward(const asset &quantity) {
+      require_auth( _self );
+      check(quantity.symbol == core_symbol, "symbol precision mismatch");
+      check(quantity.amount > 0, "amount must be a positive");
+
+      p_reward_table.set(prodsreward{quantity}, _self);
    }
 
    void auth::_addkey(const name& account, const public_key& key,
                       const string& extra_key, const name& payer) {
 
-      authkeys_tbl.emplace(payer, [&](auto &k) {
+      authkeys_tbl.emplace(_self, [&](auto &k) {
          k.N = authkeys_tbl.available_primary_key();
          k.owner = account;
          k.key = key;
@@ -112,33 +80,11 @@ namespace eosio {
          k.not_valid_after = current_time_point() + key_lifetime;
          k.revoked_at = 0; // if not revoked == 0
       });
-      to_rewards(payer, prods_reward);
+      to_rewards(payer, prods_reward.quantity);
    }
-
-   void auth::transfer(const name &from, const name &to, const asset &quantity,
-                       const public_key &key, const signature &signed_by_key) {
-
-      auto wait_idx = wait_confirm_tbl.get_index<"byname"_n>();
-      checksum256 digest = sha256(join( { from.to_string(), to.to_string(), quantity.to_string(),
-                                          string(key.data.begin(), key.data.end()) } ));
-
-      require_app_auth(digest, from, key, signed_by_key);
-
-      token::transfer_action transfer(system_token_account, {from, "active"_n});
-      action transfer_action = transfer.to_action(from, to, quantity, string("auth_app transfer"));
-      auto it = get_wait_action_it(from, system_token_account, "transfer"_n, transfer_action.data);
-      if (it != wait_idx.end()) {
-         boost_deferred_tx(it, transfer_action, key);
-      } else {
-         const time_point ct = current_time_point();
-         add_wait_action(from, system_token_account, "transfer"_n, transfer_action.data, key, ct);
-         send_deferred_tx(transfer_action, wait_confirm_sec, ct.sec_since_epoch() + from.value);
-      }
-   }
-
 
    void auth::require_app_auth(const checksum256 &digest, const name &user,
-                               const public_key &sign_pub_key, const signature &signature) {
+                               const public_key &pubkey, const signature &signature) {
 
       auto authkeys_idx = authkeys_tbl.get_index<"byname"_n>();
       auto it = authkeys_idx.find(user.value);
@@ -148,39 +94,20 @@ namespace eosio {
       for(; it != authkeys_idx.end(); ++it) {
          auto ct = current_time_point();
 
-         bool is_before_time_valid = ct < it->not_valid_before.to_time_point();
-         bool is_after_time_valid = ct > it->not_valid_after.to_time_point();
+         bool is_before_time_valid = ct > it->not_valid_before.to_time_point();
+         bool is_after_time_valid = ct < it->not_valid_after.to_time_point();
          bool is_revoked = it->revoked_at && it->revoked_at < ct.sec_since_epoch();
 
-         if ( is_before_time_valid || is_after_time_valid || is_revoked) {
+         if (!is_before_time_valid || !is_after_time_valid || is_revoked) {
             continue;
-         } else if (it->key == sign_pub_key) {
+         } else if (it->key == pubkey) {
             break;
          }
       }
       check(it != authkeys_idx.end(), "account has no active app keys");
-      assert_recover_key(digest, signature, sign_pub_key);
-   }
 
-   void auth::send_deferred_tx( const action &act, const uint32_t &delay, const uint128_t &id ) {
-      transaction t;
-      t.actions.emplace_back(act);
-      t.delay_sec = delay;
-      t.send(id, _self);
-   }
-
-   void auth::add_wait_action( const name &account, const name &action_account, const name &action_name,
-                               const vector<char> action_data, const public_key &sign_pubkey, const time_point &ct ) {
-      wait_confirm_tbl.emplace(_self, [&](auto &w) { // TODO: change _self to account
-         w.key = wait_confirm_tbl.available_primary_key();
-         w.owner = account;
-         w.init_time = ct;
-         w.wait_time = ct + wait_confirm_time;
-         w.action_account = action_account;
-         w.action_name = action_name;
-         w.action_data = action_data;
-         w.provided_approvals.push_back(sign_pubkey);
-      });
+      public_key expected_pubkey = recover_key(digest, signature);
+      check(expected_pubkey == pubkey, "expected key different than recovered user key");
    }
 
    void auth::to_rewards(const name &payer, const asset &quantity) {
@@ -197,10 +124,10 @@ namespace eosio {
 
    void auth::cleartable( ) {
       require_auth(_self);
-      for (auto _table_itr = wait_confirm_tbl.begin(); _table_itr != wait_confirm_tbl.end();) {
-         _table_itr = wait_confirm_tbl.erase(_table_itr);
+      for (auto _table_itr = authkeys_tbl.begin(); _table_itr != authkeys_tbl.end();) {
+         _table_itr = authkeys_tbl.erase(_table_itr);
       }
    }
 } /// namespace eosio
 
-EOSIO_DISPATCH( eosio::auth, (addkeyacc)(addkeyapp)(addkeywrap)(transfer)(cleartable) )
+EOSIO_DISPATCH( eosio::auth, (addkeyacc)(addkeyapp)(transfer)(setbpreward)(cleartable) )
