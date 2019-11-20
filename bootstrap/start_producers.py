e96041e47350b786c29c1723f51f241c38171f86
Artem Saprykin <asaprykin17@gmail.com>
2019-11-20 14:45:15 +0200

    multiple prods in one node

--- bootstrap/start_producers.py
+++ bootstrap/start_producers.py
@@ -2,12 +2,20 @@ from testnet_bootstrap import producers
 from start_bootnode import start_node
 
 producers_quantity = 3
+nodes_quantity = 0
 
 
-def start_producers():
-    for i in range(1, producers_quantity+1):
+def start_nodes():
+    for i in range(1, nodes_quantity+1):
         start_node(i, producers['producers'][i-1])
 
 
+def start_producers():
+    start_node(1, producers['producers'][0:producers_quantity])
+
+
 if __name__ == '__main__':
-    start_producers()
+    if nodes_quantity:
+        start_nodes()
+    if producers_quantity:
+        start_producers()
