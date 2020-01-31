0385f82bc9f7ec130198415a97839c441401354b
Art17 <ASaprykin97@gmail.com>
2020-01-31 16:13:30 +0200

    adjust rem weight formula (#129)
    * adjust rem weight formula
    
    * fix tests

--- contracts/contracts/rem.system/src/voting.cpp
+++ contracts/contracts/rem.system/src/voting.cpp
@@ -133,8 +133,9 @@ namespace eosiosystem {
    double system_contract::stake2vote( int64_t staked, time_point locked_stake_period ) const {
       check(locked_stake_period != time_point(), "vote should have mature time");
 
-      const auto weeks_to_mature = fmax( ((locked_stake_period - current_time_point()) - eosio::days(7)).count() / eosio::days(7).count(), 0.0 );
-      const auto rem_weight = 1.0 - (weeks_to_mature) / (_gremstate.stake_lock_period.count() / eosio::days(7).count());
+      const auto weeks_to_mature = fmax( ((locked_stake_period - current_time_point())).count() / eosio::days(7).count(), 0 );
+      const auto rem_weight = 1.0 - (weeks_to_mature * 7) / (_gremstate.stake_lock_period.count() / eosio::days(1).count());
+      
       const double eos_weight = std::pow( 2, int64_t((current_time_point().sec_since_epoch() - (block_timestamp::block_timestamp_epoch / 1000)) / (seconds_per_day * 7)) / double(52) );
 
       const auto vote_weight = double(staked) * eos_weight * rem_weight;
